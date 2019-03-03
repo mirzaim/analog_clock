0bc07d3ad54552e4dd28db5805929581044957d0
Morteza <mortezamirzai120@gmail.com>
2019-03-03 20:15:04 +0330

    Add test package for testing program

---
+++ src/test/ClockUITest.java
@@ -0,0 +1,10 @@
+package test;
+
+import org.clock.ui.ClockUI;
+
+public class ClockUITest {
+
+    public static void main(String[] args) {
+        ClockUI clock = new ClockUI(8, 14, 3);
+    }
+}
