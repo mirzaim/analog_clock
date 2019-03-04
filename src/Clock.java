fb19ebb1d343f304446b49e439eadb5d930c053a
Morteza <mortezamirzai120@gmail.com>
2019-03-04 09:46:01 +0330

    get Gmt time from pc

--- src/Clock.java
+++ src/Clock.java
@@ -2,14 +2,20 @@ import org.clock.logic.ClockLogic;
 import org.clock.logic.TimeZone;
 import org.clock.ui.ClockUI;
 
+import java.util.Calendar;
 import java.util.concurrent.TimeUnit;
 
 public class Clock {
 
     public static void main(String[] args) throws InterruptedException {
-        ClockLogic greenwichTime = new ClockLogic(13, 6, 0);
-        ClockLogic tehranTime = new ClockLogic(13, 6, 0, new TimeZone(3, 30, 1));
-        ClockLogic newYorkTime = new ClockLogic(13, 6, 0, new TimeZone(5, 0, -1));
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeZone(java.util.TimeZone.getTimeZone("GMT"));
+
+
+        ClockLogic greenwichTime = new ClockLogic(calendar.get(Calendar.HOUR), calendar.get(Calendar.MINUTE), 0);
+        ClockLogic tehranTime = new ClockLogic(calendar.get(Calendar.HOUR), calendar.get(Calendar.MINUTE), 0, new TimeZone(3, 30, 1));
+        ClockLogic newYorkTime = new ClockLogic(calendar.get(Calendar.HOUR), calendar.get(Calendar.MINUTE), 0, new TimeZone(5, 0, -1));
         ClockUI greenwich = new ClockUI();
         ClockUI tehran = new ClockUI();
         ClockUI newYork = new ClockUI();
